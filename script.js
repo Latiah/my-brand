e743b521868e247d87d22a141165ddbfbb163814
latifah <kimtifah2@gmail.com>
2024-01-28 01:03:24 +0200

    fixed my landing page

---
+++ script.js
@@ -0,0 +1,11 @@
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+function Delete() {
+  document.getElementById("row").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+}
