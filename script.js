bd0051d110635b566cca65ec1b2a549ac52d993f
latifah <kimtifah2@gmail.com>
2024-01-28 22:15:39 +0200

    improve responsiveness

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function Delete() {
   document.getElementById("row").style.display = "none";
   document.getElementById("popup").style.display = "none";
 }
+
+//navigation
+
+let menuBtn = document.getElementById("menu");
+let links = document.getElementById("links");
+
+menuBtn.addEventListener("click", function (e) {
+  links.classList.toggle("responsive");
+  menuBtn.classList.toggle("fa-x");
+});
