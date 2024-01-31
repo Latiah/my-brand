ac5fd7cea099333d7a282850df6a7c0614125ad4
latifah <kimtifah2@gmail.com>
2024-01-31 16:19:07 +0200

    feature:added contact validation and worked on blogs management

--- script.js
+++ script.js
@@ -1,21 +1,110 @@
-function openPopup() {
-  document.getElementById("popup").style.display = "block";
+
+// edit, add and delete blogs
+function validateForm(){
+  var title=document.getElementById("title").value;
+   var description = document.getElementById("description").value;
+    var photo= document.getElementById("photo").value;
+    if(title ==""){
+      alert("a title is required");
+      return false;
+    }
+     if (description == "") {
+       alert("please enter a description!");
+       return false;
+     }
+      if (photo == "") {
+    alert("upload a photo");
+        return false;
+      }
+      return true;
+}
+// function to show data
+function showData(){
+  var blogs;
+  if(localStorage.getItem("blogs")== null){
+    blogs=[];
+  }
+  else{
+    blogs=JSON.parse(localStorage.getItem("blogs"));
+  }
+    var html="";
+    blogs.forEach(function (element, index){
+html +="<tr>";
+html +="<td>"+ element.title +"</td>";
+html += "<td>" + element.description + "</td>";
+html += "<td>" + element.photo + "</td>";
+html +='<td onclick="update('+index+')">Edit <td onclick="deleteData('+index+')">Delete</td>';
+html += "</tr>";
+ });
+ document.querySelector("#blogstable tbody").innerHTML=html;
+
+  }
+  //shows all data when page  loads 
+  document.onload=showData();
+
+  function Add(){
+    if(validateForm()==true){
+      var title = document.getElementById("title").value;
+      var description = document.getElementById("description").value;
+      var photo = document.getElementById("photo").value;
+       var blogs;
+       if (localStorage.getItem("blogs") == null) {
+         blogs = [];
+       } else {
+         blogs = JSON.parse(localStorage.getItem("blogs"));
+       }
+       blogs.push({
+        title:title,
+description:description,
+photo:photo,
+       }
+       );
+       localStorage.setItem("blogs",JSON.stringify(blogs));
+       showData();
+       document.getElementById("title").value="";
+document.getElementById("description").value = "";       
+document.getElementById("photo").value = "";
+       
+    }
+  }
+function deleteData(index){
+  var blogs;
+   if (localStorage.getItem("blogs") == null) {
+     blogs = [];
+   } else {
+     blogs = JSON.parse(localStorage.getItem("blogs"));
+   }
+blogs.splice(index, 1);
+localStorage.setItem("blogs", JSON.stringify(blogs));
+showData();
 }
 
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
+function update(index){
+  document.getElementById("Submit").style.display="none";
+  document.getElementById("update").style.display = "block";
+ var blogs;
+ if (localStorage.getItem("blogs") == null) {
+   blogs = [];
+ } else {
+   blogs = JSON.parse(localStorage.getItem("blogs"));
+ }
+ document.getElementById("title").value = blogs[index].title;
+ document.getElementById("description").value = blogs[index].description;
+ document.getElementById("photo").value = blogs[index].photo;
+    document.querySelector("#update").onclick=function(){
+      if(validateForm()==true){
+        blogs[index].title = document.getElementById("title").value; 
+        blogs[index].description = document.getElementById("description").value; 
+        blogs[index].photo = document.getElementById("photo").value; 
+      localStorage.setItem("blogs", JSON.stringify(blogs));
+      showData();
+     document.getElementById("title").value = "";
+     document.getElementById("description").value = "";
+     document.getElementById("photo").value = "";
+     //hide update and return to add  
+     document.getElementById("Submit").style.display = "block";
+       document.getElementById("update").style.display = "none";
+      }
+    }   
+
 }
-function Delete() {
-  document.getElementById("row").style.display = "none";
-  document.getElementById("popup").style.display = "none";
-}
-
-//navigation
-
-let menuBtn = document.getElementById("menu");
-let links = document.getElementById("links");
-
-menuBtn.addEventListener("click", function (e) {
-  links.classList.toggle("responsive");
-  menuBtn.classList.toggle("fa-x");
-});
